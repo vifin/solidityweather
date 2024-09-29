8e5f7b7d6f02a9fddfecd9da7a11ffab89490a75
Viktor <vikinecko@gmail.com>
2024-09-29 13:51:46 +0400

    Changed to english

--- helloworld.sol
+++ helloworld.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.0;
 contract HelloWorld {
     function getWeatherStatus(uint temperature) public pure returns (string memory) {
         if (temperature > 20) {
-            return "Teplo";
+            return "Cold";
         } else {
-            return "Zima";
+            return "Warm";
         }
     }
 
     function a() public pure {
-        getWeatherStatus(2);
+        getWeatherStatus(4);
     }
 
 }
